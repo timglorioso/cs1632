d04b60965e7544a2582bd68fc50166947895a179
timglorioso <glorioso.tim@gmail.com>
2016-02-14 17:51:11 -0500

    rename test case class, delete contents

---
+++ 2/src/CoffeeMakerQuestTest.java
@@ -0,0 +1,7 @@
+import org.junit.Test;
+import org.junit.Assert;
+import org.mockito.Mockito.*;
+
+public class CoffeeMakerQuestTest {
+
+}
--- 2/src/CoffeeMakerQuestTests.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-import static org.mockito.Mockito.*;
-
-public class CoffeeMakerQuestTests {
-
-    public static void main(String[] args) {
-        System.out.println("A coffee test.");
-    }
-}
