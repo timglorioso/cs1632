9ce6b1cd6c2f7631cc71e08db56d6729a68a6d72
timglorioso <glorioso.tim@gmail.com>
2016-02-15 02:44:57 -0500

    add FUN-ITERATION unit test (red)

--- 2/src/CoffeeMakerQuestTest.java
+++ 2/src/CoffeeMakerQuestTest.java
@@ -5,4 +5,14 @@ import static org.mockito.Mockito.*;
 
 public class CoffeeMakerQuestTest {
 
+    /*
+     * see requirement: FUN-ITERATION
+     */
+    @Test
+    public void testAcceptsValidCommand() {
+        CoffeeMakerQuest testGame = new CoffeeMakerQuest();
+        String command = testGame.getNextCommand();
+        assertThat(command, anyOf(equalTo("N"), equalTo("S"), equalTo("L"),
+                                  equalTo("I"), equalTo("H"), equalTo("D")));
+    }
 }
