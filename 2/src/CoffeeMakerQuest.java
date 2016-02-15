76a00d8e18bca307392b21ad8b4ba32ad965b42e
timglorioso <glorioso.tim@gmail.com>
2016-02-15 02:47:50 -0500

    add nextCommmand field and getter (green)

--- 2/src/CoffeeMakerQuest.java
+++ 2/src/CoffeeMakerQuest.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class CoffeeMakerQuest {
 
+    private String nextCommand;
+
+    public CoffeeMakerQuest() {
+        nextCommand = "H";
+    }
+
     public static void main(String[] args) {
-        System.out.println("I like coffee.");
+    }
+
+    public String getNextCommand() {
+        return nextCommand;
     }
 }
