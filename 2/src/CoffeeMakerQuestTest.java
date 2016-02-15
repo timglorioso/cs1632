67ed9c7ec06adacea5eee95a11faf41a4baf042d
timglorioso <glorioso.tim@gmail.com>
2016-02-15 00:07:41 -0500

    adjust imports in test class

--- 2/src/CoffeeMakerQuestTest.java
+++ 2/src/CoffeeMakerQuestTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
-import org.junit.Assert;
-import org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.*;
 
 public class CoffeeMakerQuestTest {
 
