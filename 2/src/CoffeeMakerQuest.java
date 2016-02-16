f6240f71e3ea14b4cfa0414c3e52f959ad90eae8
timglorioso <glorioso.tim@gmail.com>
2016-02-15 19:12:08 -0500

    refactor CoffeeMakerQuest

--- 2/src/CoffeeMakerQuest.java
+++ 2/src/CoffeeMakerQuest.java
@@ -1,6 +1,5 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class CoffeeMakerQuest {
 
@@ -11,10 +10,6 @@ public class CoffeeMakerQuest {
     }
 
     public static void main(String[] args) {
-    }
-
-    public List<String> getValidCommands() {
-        return validCommands;
     }
 
     public String respondToCommand(String command) {
