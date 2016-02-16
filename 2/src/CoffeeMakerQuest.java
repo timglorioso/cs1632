de12a92ee8b3b693d8be8a3b9da963bcf37bda8f
timglorioso <glorioso.tim@gmail.com>
2016-02-16 03:17:50 -0500

    respond to all possible commands (green)

--- 2/src/CoffeeMakerQuest.java
+++ 2/src/CoffeeMakerQuest.java
@@ -4,18 +4,42 @@ import java.util.List;
 public class CoffeeMakerQuest {
 
     private List<String> validCommands;
+    private List<String> validCommandResponses;
+    private String unknownCommandResponse;
 
     public CoffeeMakerQuest() {
         validCommands = Arrays.asList("N", "S", "L", "I", "H", "D");
+        validCommandResponses = Arrays.asList(
+            "",
+            "",
+            "",
+            "",
+            String.join(System.getProperty("line.separator"),
+                "The following single-character commands are at your disposal:",
+                "N - move north, if possible",
+                "S - move south, if possible",
+                "L - look around the room for ingredients to pick up",
+                "I - check your inventory",
+                "D - mix the ingredients in your inventory and drink the result",
+                "H - display this help message"),
+            "");
+        unknownCommandResponse = "What?";
     }
 
     public static void main(String[] args) {
     }
 
     public String respondToCommand(String command) {
-        String response = new String();
-        if (!validCommands.contains(command.toUpperCase())) {
-            response = "What?";
+        String response = null;
+        command = command.toUpperCase();
+        for (int i = 0; i < validCommands.size(); i++) {
+            if (validCommands.get(i).equals(command)) {
+                response = validCommandResponses.get(i);
+                break;
+            }
+        }
+        if (response == null) {
+            response = unknownCommandResponse;
         }
         return response;
     }
