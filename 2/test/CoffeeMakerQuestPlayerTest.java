a9ffaed4d389c637613f0ed3c585fa963c84febe
timglorioso <glorioso.tim@gmail.com>
2016-02-16 12:53:15 -0500

    add player inventory unit tests (red)

--- 2/test/CoffeeMakerQuestPlayerTest.java
+++ 2/test/CoffeeMakerQuestPlayerTest.java
@@ -6,7 +6,21 @@ import static org.mockito.Mockito.*;
 public class CoffeeMakerQuestPlayerTest {
 
     @Test
-    public void testStringifiesInventory() {
+    public void testStringifiesEmptyInventory() {
         CoffeeMakerQuestPlayer testPlayer = new CoffeeMakerQuestPlayer();
+        String inventoryString = testPlayer.inventoryToString();
+        assertEquals("nothing", inventoryString);
+    }
+
+    @Test
+    public void testStringifiesNonEmptyInventory() {
+        CoffeeMakerQuestPlayer testPlayer = new CoffeeMakerQuestPlayer();
+        testPlayer.addToInventory("cream");
+        testPlayer.addToInventory("sugar");
+        testPlayer.addToInventory("coffee");
+        String inventoryString = testPlayer.inventoryToString();
+        assertThat(inventoryString, anyOf(containsString("coffee"),
+                                          containsString("cream"),
+                                          containsString("sugar")));
     }
 }
