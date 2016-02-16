05f72eaedafb122da42a4b6ba4e04a593b9fdebe
timglorioso <glorioso.tim@gmail.com>
2016-02-16 12:54:17 -0500

    add inventoryToString() method (green)

--- 2/src/CoffeeMakerQuestPlayer.java
+++ 2/src/CoffeeMakerQuestPlayer.java
@@ -1,6 +1,23 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class CoffeeMakerQuestPlayer {
 
     private List<String> inventory;
+
+    public CoffeeMakerQuestPlayer() {
+        inventory = new ArrayList<String>();
+    }
+
+    public void addToInventory(String item) {
+        inventory.add(item);
+    }
+
+    public String inventoryToString() {
+        String inventoryString = "nothing";
+        if (!inventory.isEmpty()) {
+            inventoryString = String.join(", ", inventory);
+        }
+        return inventoryString;
+    }
 }
