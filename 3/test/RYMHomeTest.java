f583c2dc0a391f4f9d1cc47cdc87e14bfcc327dc
timglorioso <glorioso.tim@gmail.com>
2016-02-27 17:54:53 -0500

    setup emtpy RYM test, delete RedditTest.java

---
+++ 3/test/RYMHomeTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class RYMHomeTest {
+
+    static WebDriver driver = new HtmlUnitDriver();
+
+    @Before
+    public void setUp() throws Exception {
+        // driver.get("https://rateyourmusic.com/");
+    }
+
+    @Test
+    public void testNothing() {
+    }
+}
