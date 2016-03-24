93ae1d519c99d4def012cc7ed49d7c5820f38993
timglorioso <glorioso.tim@gmail.com>
2016-03-24 13:51:45 -0400

    generate random arrays of numbers

--- 4/test/SortTest.java
+++ 4/test/SortTest.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.stream.IntStream;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
@@ -6,8 +9,23 @@ import org.junit.Test;
 
 public class SortTest {
 
+    static int[][] arraysOfNumbers;
+
     @BeforeClass
     public static void setUpOnce() {
+        Random rng = new Random();
+
+        // create an array of random array sizes
+        int numberOfArrays = 100;
+        IntStream randomSizes = rng.ints(numberOfArrays, 1, 1000);
+        int[] arraySizes = randomSizes.toArray();
+
+        // create arrays of random numbers
+        arraysOfNumbers = new int[numberOfArrays][];
+        for (int i = 0; i < arraysOfNumbers.length; i++) {
+            IntStream randomNumbers = rng.ints(arraySizes[i], -1000, 1000);
+            arraysOfNumbers[i] = randomNumbers.toArray();
+        }
     }
 
     @Test
