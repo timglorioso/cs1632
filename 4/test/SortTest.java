e8ea809a458c31a1da67f992bc9cf89208f20712
timglorioso <glorioso.tim@gmail.com>
2016-03-23 23:23:41 -0400

    add project 4 build file and empty test class

---
+++ 4/test/SortTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SortTest {
+
+    @BeforeClass
+    public static void setUpOnce() {
+    }
+
+    @Test
+    public void doNothing() {
+    }
+}
